19684058e0e0f2ac10ef9dbabad0b0029f83d92b
Wyman <denisgorbatuk57@gmail.com>
2024-02-03 09:45:51 +0500

    Initial commit

---
+++ SPTaskThreeThree.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+
+	double Pi = 3.14159265358979323846, a;
+
+	cout << "Введите a: ";
+	cin >> a;
+
+	double y = sin(Pi / 2 + 3 * a) / (1 - sin(3 * a - Pi));
+
+	cout << endl << "Результат sin(П / 2 + 3 * а) / (1 - sin(3 * а - П)) = " << y;
+}
